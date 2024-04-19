8c56d20816c93a95629d7f39f1a4c2138ab88749
student <student@cpto.ujep.cz>
2024-04-19 16:48:59 +0200

    neco jsem udelal

--- add.py
+++ add.py
@@ -1,2 +1,31 @@
-def add(a, b):
-    return a + b
+class Kalkulacka:
+
+    def secti(self, a, b):
+        return a + b
+
+    def odecti(self, a, b):
+        return a - b
+    def answer_message(contact, message):
+        """
+
+        :param message:
+        :return:
+        """
+    def vynasob(self, a, b):
+        return a * b
+    def answer_message(contact, message):
+        """
+
+        :param message:
+        :return:
+        """
+    def vydel(self, a, b):
+
+
+        docstring:
+def answer_message (contact, message):
+    """
+    :param contact:
+    :param message:
+    :return:
+    """
